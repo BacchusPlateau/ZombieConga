e4a1fc670e59e6590b6dc2df31eae8437be84e2b
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-08-18 15:14:45 -0500

    ch1

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -13,7 +13,16 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         backgroundColor = UIColor.black
-    
+        
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.zPosition = -1
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(background)
+        
+        let zombieOne = SKSpriteNode(imageNamed: "zombie1")
+        zombieOne.position = CGPoint(x: 400, y: 400)
+        addChild(zombieOne)
+        
     }
     
 }
